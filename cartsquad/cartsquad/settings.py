d00c585952669cc279be1f1e7ea08927effbe8d6
Mouli Lukalapu <moulinaidu.lukalapu@slu.edu>
2023-10-07 10:55:20 -0500

    MySQL Integration

--- cartsquad/cartsquad/settings.py
+++ cartsquad/cartsquad/settings.py
@@ -73,12 +73,19 @@ WSGI_APPLICATION = 'cartsquad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cartsquad',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
+
 
 
 # Password validation
