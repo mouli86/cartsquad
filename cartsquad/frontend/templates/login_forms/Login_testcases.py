46e78c626f51b9eee5aa98020f12f8c50070ac1e
Srupireddy1405 <145719503+Srupireddy1405@users.noreply.github.com>
2023-10-27 14:41:54 -0400

    Updated Login_testcases.py

--- cartsquad/frontend/templates/login_forms/Login_testcases.py
+++ cartsquad/frontend/templates/login_forms/Login_testcases.py
@@ -9,7 +9,7 @@ def test_password_length():
     assert check_password_length("short") == False
     assert check_password_length("eight888") == True
     assert check_password_length("password123") == True
-    assert check_password_length("verylongpassword") == True
+    assert check_password_length("verylongpassword") == True #which checks if the length of the password is greater than or equal to 8 characters.
 
 if __name__ == "__main__":
     pytest.main()
