ab07db1e5c1ebd6b752bdbd9155a2a87aa9543f1
srupireddy1405 <sahithi.rupireddy@slu.edu>
2023-10-27 15:00:04 -0400

    Adding User Login and SignUp html pages

--- cartsquad/cartsquad/settings.py
+++ cartsquad/cartsquad/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cartsquad',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'password',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
