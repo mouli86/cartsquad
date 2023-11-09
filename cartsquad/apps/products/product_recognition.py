b88d2d53a2b03b69a245cc761c75fd441bbb4862
PSD <mayukyadav31@gmail.com>
2023-11-08 20:15:09 -0600

    CNN model optimization

--- cartsquad/apps/products/product_recognition.py
+++ cartsquad/apps/products/product_recognition.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from sklearn.model_selection import train_test_split
 import pickle
 
-# Load the Fashion MNIST dataset
+# Fashion MNIST dataset
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 
 # Define class labels
@@ -21,7 +21,7 @@ X_test = (X_test - mean) / std
 # Split the data into training and validation sets
 X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
-# Create a complex CNN model with more layers
+# CNN model
 cnn_model = keras.models.Sequential([
     keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
@@ -31,30 +31,37 @@ cnn_model = keras.models.Sequential([
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
     keras.layers.Conv2D(256, kernel_size=(3, 3), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(512, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
     keras.layers.Flatten(),
+    keras.layers.Dense(1024, activation='relu'),
+    keras.layers.Dropout(0.4),
     keras.layers.Dense(512, activation='relu'),
     keras.layers.Dropout(0.4),
     keras.layers.Dense(256, activation='relu'),
     keras.layers.Dropout(0.4),
     keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(16, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
 # Compile the model
 cnn_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model with more epochs and larger batch size
+# Train the model with more epochs and a larger batch size
 cnn_model.fit(X_train, y_train, epochs=25, batch_size=256, validation_data=(X_validation, y_validation))
 
 # Evaluate the model
 test_loss, test_accuracy = cnn_model.evaluate(X_test, y_test)
 print("Test Accuracy:", test_accuracy)
 
-# Function to make predictions and return the predicted label
+# Function to return the predicted label
 def make_prediction_and_return_label(test_image):
     # Make a prediction
     y_pred = cnn_model.predict(np.expand_dims(test_image, axis=0)).round(2)
-    predicted_label = class_labels[np.argmax(y_pred[0])]
+    predicted_label = class_labels[np.argmax(y_pred[0)]
     return predicted_label
 
 # Save the model
