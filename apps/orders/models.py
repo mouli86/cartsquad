6f0742ee814df726afd0c5ba5b905f752834b865
mayuk123 <mayukyadav31@gmail.com>
2023-11-10 19:00:21 -0600

    Comments added to code

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -2,7 +2,10 @@ from django.db import models
 import datetime as dt
 
 class Orders(models.Model):
+    # Primary key for the Orders model, automatically generated as a large integer.
     seq_id = models.BigAutoField(primary_key=True)
+
+    # Fields representing attributes of an order.
     order_id = models.IntegerField()
     user_id = models.IntegerField()
     product_id = models.IntegerField()
@@ -10,11 +13,14 @@ class Orders(models.Model):
     per_item_cost = models.DecimalField(decimal_places=2, max_digits=10)
     total_cost = models.DecimalField(decimal_places=2, max_digits=10)
 
+    # Fields that are required for creating an order.
     REQUIRED_FIELDS = ['order_id', 'user_id', 'quantity', 'product_id', 'total_cost', 'per_item_cost']
 
+    # String representation of the Orders instance.
     def __str__(self):
         return f"{self.product_id}, {self.user_id}, {self.order_id}"
 
+    # Override the save method to set additional fields before saving.
     def save(self, *args, **kwargs):
         today = dt.date.today()
         self.product_date_modified = today
@@ -22,6 +28,7 @@ class Orders(models.Model):
         self.product_status = True
         super().save(*args, **kwargs)
 
+    # Method to add orders with specified attributes.
     def add_orders(self, order_id, user_id, product_id, quantity, per_item_cost, total_cost):
         self.order_id = order_id
         self.user_id = user_id
@@ -30,19 +37,23 @@ class Orders(models.Model):
         self.per_item_cost = per_item_cost
         self.total_cost = total_cost
         self.save()
-
+        
 
 class OrdersIterator:
+    # Initialize the iterator with a list of orders and set the index to 0.
     def __init__(self, orders):
         self.orders = orders
         self.index = 0
 
+    # Make the iterator itself iterable.
     def __iter__(self):
         return self
 
+    # Get the next order in the list.
     def __next__(self):
         if self.index < len(self.orders):
             result = self.orders[self.index]
             self.index += 1
             return result
+        # Raise StopIteration when there are no more orders.
         raise StopIteration
--- apps/orders/tests.py
+++ apps/orders/tests.py
@@ -6,6 +6,7 @@ from datetime import date
 from decimal import Decimal
 
 class OrdersTestCase(TestCase):
+    # Setting up initial data for testing
     def setUp(self):
         Orders.objects.create(
             order_id=1,
@@ -16,10 +17,12 @@ class OrdersTestCase(TestCase):
             total_cost=Decimal('30.00')
         )
 
+    # Test the __str__ method of the Orders model
     def test_orders_model_str_method(self):
         order = Orders.objects.get(order_id=1)
         self.assertEqual(str(order), "201, 101, 1")
 
+    # Test the save method of the Orders model
     def test_orders_model_save_method(self):
         order = Orders.objects.get(order_id=1)
         today = date.today()
@@ -27,21 +30,26 @@ class OrdersTestCase(TestCase):
         self.assertEqual(order.product_date_added, today)
         self.assertTrue(order.product_status)
 
+    # Test the custom iterator for Orders
     def test_orders_iterator(self):
         orders = Orders.objects.all()
         orders_iterator = OrdersIterator(orders)
         result = [order for order in orders_iterator]
         self.assertEqual(len(result), 1)
 
+    # Test the view_order_history view function
     def test_view_order_history(self):
         response = self.client.get(reverse('orders:view_order_history', args=(101,)))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "201, 101, 1")
 
+    # Test the add_to_orders view function
     def test_add_to_orders(self):
+        # Simulate a request to add an order to the cart
         cart_response = self.client.get(reverse('orders:add_to_orders', args=(101, 1)))
         self.assertEqual(cart_response.status_code, 200)
 
+        # Retrieve the added order from the database and check its attributes
         order = Orders.objects.get(order_id=1)
         self.assertEqual(order.user_id, 101)
         self.assertEqual(order.product_id, 201)
