fa41da974551701a6d7dca38a461c6f391a37f0d
sbaru01 <145718724+sbaru01@users.noreply.github.com>
2023-11-27 10:53:54 -0600

    Updated forms.py

--- cartsquad/apps/accounts/forms.py
+++ cartsquad/apps/accounts/forms.py
@@ -6,10 +6,21 @@ from django.contrib.auth import authenticate
 
 #Customer registration form
 class CustomerRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Valid email required." )
+    email = forms.EmailField(required=True)
+
     class Meta:
-        model = Account  # User registration form using the Account model
+        model = Account
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2', 'phone_number', 'gender', 'date_of_birth']
+        
+    def __init__(self, *args, **kwargs):
+        super(CustomerRegistrationForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' in field.widget.attrs:
+                field.widget.attrs['class'] += ' input'
+            else:
+                field.widget.attrs['class'] = 'input'
+
 
 #Retailer registration form
 class RetailerRegistrationForm(UserCreationForm):
@@ -17,6 +28,15 @@ class RetailerRegistrationForm(UserCreationForm):
     class Meta:
         model = Account  # Retailer registration form using the Account model
         fields = [ 'business_name', 'retailer_name', 'email', 'phone_number', 'retailer_address','password1', 'password2', 'is_retailer']
+    
+    def __init__(self, *args, **kwargs):
+        super(RetailerRegistrationForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' in field.widget.attrs:
+                field.widget.attrs['class'] += ' input'
+            else:
+                field.widget.attrs['class'] = 'input'
 
 #User login form
 class UserLoginForm(forms.ModelForm):
@@ -26,6 +46,15 @@ class UserLoginForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ('email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' in field.widget.attrs:
+                field.widget.attrs['class'] += ' input'
+            else:
+                field.widget.attrs['class'] = 'input'
 
     def clean(self):
         if self.is_valid():
@@ -53,3 +82,12 @@ class RetailerLoginForm(forms.ModelForm):
             password = self.cleaned_data['password']
             if not authenticate(email = email, password = password):
                 raise forms.ValidationError("Please check your email and password and try again.")
+            
+    def __init__(self, *args, **kwargs):
+        super(RetailerLoginForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' in field.widget.attrs:
+                field.widget.attrs['class'] += ' input'
+            else:
+                field.widget.attrs['class'] = 'input'
