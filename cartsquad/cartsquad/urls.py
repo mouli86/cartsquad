ffa63cb3fb1a7532903f42a7fde8405e824bf88e
sbaru01 <145718724+sbaru01@users.noreply.github.com>
2023-11-28 18:02:56 -0600

    Update urls.py

--- cartsquad/cartsquad/urls.py
+++ cartsquad/cartsquad/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,8 @@ urlpatterns = [
     path('cart/', include('apps.cart.urls')),
     path('orders/', include('apps.orders.urls')),
 ]
+
+handler404 = views.custom_404
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
